aa6fe88ccc9cfe667f6efbe6a5e23896f318c44d
Karenolivia13 <karenolivem13@gmail.com>
2021-06-18 14:11:31 +0300

    ft-rest-api-structure

---
+++ api-database/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const hostname = 'localhost';
+const port = process.env.PORT || 8080
+
+server.use(express.json()); 
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/', (req, res)=> res.send('Safe Courier RESTful API'));
+server.get('/data', (req, res)=> res.json({parcelId: "1", parcel: "Toothbrush"}))
+server.listen(port, () => {
+    console.log(`Server is running at http://${hostname}:${port}`); 
+});
